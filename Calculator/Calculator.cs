b45fa9fca1e540f4e60f03ea07a4724b772310cc
Devon <devon.coates@totaleto.com>
2023-08-03 17:36:03 -0600

    Created basic calculator console app

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿namespace Program
+{
+    public class Calculator
+    {
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+        public static float Multiply(float x, float y)
+        {
+            return (x * y);
+        }
+        public static float Divide(float x, float y)
+        {
+            return (x / y);
+        }
+
+        public static float? PerformOperation(float x, float y, string option)
+        {
+            if (String.IsNullOrWhiteSpace(option))
+            {
+                Console.WriteLine("Please Provide a valid option");
+                return null;
+            }
+            switch (option)
+            {
+                case "a": return Add(x, y);
+                case "s": return Sub(x, y);
+                case "m": return Multiply(x, y);
+                case "d": return Divide(x, y);
+                default:
+                    return null;
+            }
+
+        }
+
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var running = true;
+            while (running)
+            {
+                float? num1 = null;
+                float? num2 = null;
+
+                Console.WriteLine("Console Calculator in C#\r");
+                Console.WriteLine("------------------------\n");
+
+                Console.WriteLine("Type a number, and then press Enter.");
+                var firstInput = Console.ReadLine();
+
+                if (float.TryParse(firstInput, out float firstInputResult))
+                {
+                    num1 = float.Parse(firstInput);
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter a Valid Number");
+                }
+
+
+
+                Console.WriteLine("Type another number, and then press Enter");
+                var secondInput = Console.ReadLine();
+
+                if (float.TryParse(secondInput, out float secInputResult))
+                {
+                    num2 = (float)float.Parse(secondInput);
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter a Valid Number");
+                }
+
+                if (num1 != null && num2 != null)
+                {
+                    // Ask the user to choose an option.
+                    Console.WriteLine("Choose an option from the following list:");
+                    Console.WriteLine("\ta - Add");
+                    Console.WriteLine("\ts - Subtract");
+                    Console.WriteLine("\tm - Multiply");
+                    Console.WriteLine("\td - Divide");
+                    Console.Write("Your option? ");
+
+                    var operation = Console.ReadLine();
+
+                    var result = Calculator.PerformOperation((float)num1, (float)num2, operation);
+                    Console.WriteLine($"Result: {result.ToString()}");
+                }
+            }
+        }
+    }
+}
